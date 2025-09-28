bc4fb80a90a08497c2c198ced1bc66706ddb3a45
peguthierres <peguthierres@outlook.com>
2025-09-28 10:30:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { runDiagnostics } from './utils/diagnostics';
-
-// Executar diagnóstico em produção também
-if (import.meta.env.PROD) {
-  runDiagnostics();
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
